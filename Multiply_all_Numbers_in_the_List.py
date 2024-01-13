4af7b22815f2f77f4bdb733ff685f3a12b8683b3
anr0510 <142798019+anr0510@users.noreply.github.com>
2024-01-13 11:29:22 +0530

    This code is tested to Multiply all numbers in the list.

---
+++ Multiply_all_Numbers_in_the_List.py
@@ -0,0 +1,9 @@
+# list = [10,20,30,40,50]
+list = [1,2,3,4,5]
+
+multiplication = 1
+
+for n in list:
+    multiplication *= n
+
+print(f"The multiplication of all the numbers in the list is: {multiplication}")
