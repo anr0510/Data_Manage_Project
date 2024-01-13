1c756232d395661e79dbf1c595d31272d4444a50
anr0510 <142798019+anr0510@users.noreply.github.com>
2024-01-13 13:24:14 +0530

    This code is tested to find Even Numbers from a list.

--- Find_Even_Numbers_in_List.py
+++ Find_Even_Numbers_in_List.py
@@ -6,3 +6,16 @@ even_numbers = [num for num in numbers if num % 2 == 0]
 
 # Print the even numbers
 print("Even numbers in the list:", even_numbers)
+
+# Below and above code both used to find the even numbers
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        pass  # For odd numbers, do nothing
+
+print("Even numbers in the list:", even_numbers)
