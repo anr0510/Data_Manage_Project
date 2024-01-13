7618c45f09c9f764e8d3955d40dfc1ab4ad39494
anr0510 <142798019+anr0510@users.noreply.github.com>
2024-01-13 13:58:42 +0530

    This code is tested to Merge two dictionaries together. Updated Second method to merge Two Dictionary using dictionary Unpacking.

--- Merging_Two_Dictionaries.py
+++ Merging_Two_Dictionaries.py
@@ -7,3 +7,15 @@ dict1.update(dict2)
 # The merged dictionary is now in dict1
 
 print("Merged Dictionary (using update()):", dict1)
+#========================================================
+
+# 2. Using dictionary unpacking
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'd': 4}
+
+# Merge dict2 into dict1 using dictionary unpacking
+merged_dict = {**dict1, **dict2}
+
+# The merged dictionary is now in merged_dict
+print("Merged Dictionary (using dictionary unpacking):", merged_dict)
