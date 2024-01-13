55f780ec6859c018e9af724491b3f7639b807e50
anr0510 <142798019+anr0510@users.noreply.github.com>
2024-01-13 13:32:22 +0530

    This code is tested to Clone or Copy a list.

--- Cloning_or_Copying_a_List.py
+++ Cloning_or_Copying_a_List.py
@@ -1,3 +1,4 @@
+# 1. Using Using the Slice Operator
 original_list = [1, 2, 3, 4, 5]
 cloned_list = original_list[:]
 print(cloned_list)
@@ -7,3 +8,20 @@ print(cloned_list)
 
 # The elements of original_list are copied into cloned_list,
 # creating an identical but separate list.
+
+# =========================================================
+# 2. Using the list() constructor
+original_list = [1, 2, 3, 4, 5]
+cloned_list = list(original_list)
+print(cloned_list)
+
+# list(original_list) creates a new list (cloned_list)
+# based on the elements present in original_list.
+# The list() function is used here to create a new list
+# by passing the original_list as an argument.
+
+# =========================================================
+# 3. Using List Comprehension
+original_list = [1, 2, 3, 4, 5]
+cloned_list = [item for item in original_list]
+print(cloned_list)
