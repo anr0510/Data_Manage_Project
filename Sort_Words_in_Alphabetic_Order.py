034c88a5748b816dd79a67eb2d86cb0a0b68f9bd
anr0510 <142798019+anr0510@users.noreply.github.com>
2024-01-13 11:07:41 +0530

    This code is tested to Sort Words in Alphabetic Order.

---
+++ Sort_Words_in_Alphabetic_Order.py
@@ -0,0 +1,13 @@
+# Program to sort alphabetically the words form a string provided by th
+my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = [word.capitalize() for word in my_str.split()]
+print(words)
+
+words.sort()
+
+print("The sorted words are:")
+
+for word in words:
+    print(word)
